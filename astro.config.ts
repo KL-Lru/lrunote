99d24c63e4fd7c8829e625ae5b331d6f559ddee0
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-06-08 04:48:10 +0900

    astro config js to ts

---
+++ astro.config.ts
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+import starlight from '@astrojs/starlight';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    icon(),
+    starlight({
+      title: 'Lru Note',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/kl-lru/lrunote',
+        },
+      ],
+      sidebar: [],
+    }),
+  ],
+});
