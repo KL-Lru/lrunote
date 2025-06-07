15093dd6dd0bd1af3b953ae12ef3773fda46178f
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-06-07 22:48:07 +0900

    add iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    icon(),
     starlight({
       title: 'My Docs',
       social: [
